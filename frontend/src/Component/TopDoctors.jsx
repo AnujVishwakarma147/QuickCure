80038c3a1a3b8fcb7cb794a0885ccf7f06a227d9
Anuj Vishwakarma <av554289@gmail.com>
2026-07-04 22:14:56 +0530

    Import doctors data into TopDoctors component

--- frontend/src/Component/TopDoctors.jsx
+++ frontend/src/Component/TopDoctors.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import { doctors } from "../assets/assets";
 
 const TopDoctors = () => {
 
